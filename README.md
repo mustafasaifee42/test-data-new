3eb0f5211d1136e7146459261b065be2177f4f56
Mustafa Saifee <saifee.mustafa@gmail.com>
2026-03-22 19:13:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# 📦 Data Repository Template
+This repository serves as a template for organizing and managing data files across projects. It provides a standardized structure for storing, validating, and sharing datasets effectively.
+
+__[Log file](./validation.log)__
+
+
+## ✅ Automated Data Validation
+A GitHub Action is included to automatically validate uploaded data files (`.csv`, `.json`, `.xlsx`, `.xls`) whenever a commit or pull request modifies files in the `data/` folders.
+
+### 🔁 What Happens on Validation Failure
+
+If validation fails:
+* 🔍 The workflow identifies the modified data files
+* ♻️ Reverts only the modified files to their previous state
+* 💬 Commits the reversion with an appropriate message
+* ❌ Fails the workflow to prevent invalid data from being merged
+
+You can view detailed logs and errors in the `GitHub Actions` tab.
+
+
+### 🔧 Trigger Conditions
+
+This workflow runs on:
+* Push to any branch when: Files in `data/` folders with supported extensions are changed
+* Pull Requests modifying those same files
+
+
+### 🛠️ Validation Script
+
+The validation logic is handled by `validation/validate.js`.
+
+
+#### 🔍 How It Works
+
+* Looks for files in `data/` with one of the following extensions: `.csv`, `.xlsx`, `.xls`, `.json`.
+* For each data file, checks for a corresponding schema file in the schema/ directory:
+    * The schema file must have the same base name and a `.json` extension.
+    * If no schema exists, the data file is considered valid by default.
+* Validates the data against the schema.
+* Logs success or details of any validation errors in log file [here](./validation.log).
+
+
+### 📐 Schema format
+The schema file in the `schema/` folder must be a JSON array with the following structure:
+
+```ts
+{
+  columnName: string;
+  type: 'string' | 'number' | 'Alpha 3 code' | 'dateTime' | 'boolean';
+  required?: boolean;
+  enum?: string[];        // Only applicable if type is 'string'
+  range?: [number, number]; // Only applicable if type is 'number'
+  dateFormat?: string;    // Only applicable if type is 'dateTime'
+}[]
+```
+
+## Logs
+The github action creates a log file after every change to the data sheet. Log file can be found [here](./validation.log).
+
+### Example log files
+
+__Log for successful validation__
+
+```log
+
+Updated on: 7/12/2025, 10:07:54 AM UTC
+Status: ✅ Validation Successful.
+
+```
+
+
+__Log for unsuccessful validation__
+
+```log
+
+Updated on: 12/07/2025, 12:57:18 UTC
+Status: ❌ Validation failed. Files reverted to their previous valid version.
+
+---
+
+🚩 Files with issues (2)
+
+📄 data01.csv
+Row: 1 | Column: column 4 | ⚠️ Error: column 4 must be a valid number. Received: "text"
+Row: 2 | Column: column 4 | ⚠️ Error: column 4 must be a valid number. Received: "tex"
+
+📄 data02.csv
+Row: 1 | Column: column 4 | ⚠️ Error: column 4 must be a valid number. Received: "text"
+Row: 2 | Column: column 4 | ⚠️ Error: column 4 must be a valid number. Received: "yex"
+
+---
+
+✅ Files without any issues (2)
+
+📄 data03.csv
+📄 data04.csv
+
+---
+```
+
+## Related Repos
+* [name of the repo where this data is used](link to repo where this data is used): This is the application or project that uses this dataset. [View the application site]({{link to the project site here}})
+
+
+## Contact
+For questions or suggestions, please open an issue or contact the repository maintainer.
---
+++ validation/validate.js
@@ -0,0 +1,130 @@
+const { validateData, parseCsvString } = require('@undp-data/data-utils');
+const fs = require('fs');
+const path = require('path');
+
+
+const dataDir = path.resolve(__dirname, '../data');
+const schemaDir = path.resolve(__dirname, '../schema');
+const logPath = path.resolve(__dirname, '../validation.log');
+
+const allowedExtensions = ['.csv', '.xlsx', '.json', '.xls'];
+
+const files = fs.readdirSync(dataDir);
+
+let invalidFiles = [];
+let validFiles = [];
+
+
+console.log('');
+console.log('---');
+console.log('');
+console.log('Validation all data files');
+
+files.filter(file => {
+  const ext = path.extname(file).toLowerCase();
+  return allowedExtensions.includes(ext);
+}).forEach(file => {
+  
+  const filePath = path.join(dataDir, file);
+  const fileNameWithoutExt = path.basename(file, path.extname(file));
+  const fileWithJsonExt = path.format({
+    name: fileNameWithoutExt,
+    ext: '.json',
+  });
+
+  const schemaPath = path.join(schemaDir, fileWithJsonExt);
+    
+  console.log('');
+  console.log(`📊 Validating ${file}`);
+  if (fs.existsSync(schemaPath)) {
+    try {
+      const content = fs.readFileSync(filePath, 'utf8');
+      const schema = JSON.parse(fs.readFileSync(schemaPath, 'utf8'));
+      const validationErrors = validateData(parseCsvString(content), schema);
+      if(validationErrors.length !== 0) {
+        console.log('');
+        console.log(`❌ ${file} failed validation. Total ${validationErrors.length} errors:`);
+        validationErrors.forEach((el,i) => {
+          console.log(`Error ${i + 1}: ${JSON.stringify(el)}`)
+        });
+        console.log('');
+        invalidFiles.push({file, err: validationErrors});
+      } else {
+        console.log('');
+        console.log(`✅ ${file} is valid!`);
+        validFiles.push(file);
+        console.log('');
+      }      
+    } catch (error) {
+      console.log('');
+      console.log(`🔥 Error reading file ${filePath}: ${error.message}`);
+      console.log('');
+      invalidFiles.push({file, err: error.message});
+      process.exit(1);
+
+    }
+  } else {
+    console.log('');
+    console.log(`🔥 Schema file does not exist for ${file}. No validation required.`);
+    validFiles.push(file);
+    console.log('');
+  }
+});
+
+if(invalidFiles.length > 0) {
+  console.log('');
+  console.log(`❌ Following files failed validation:`);
+  invalidFiles.forEach(d => { console.log(d); });
+  console.log('');
+  console.log('---');
+  console.log('');
+  const formattedDate = new Date().toLocaleString();
+  const errorList = invalidFiles.map(d => 
+    `📄 ${d.file}\n` +
+    d.err.map(e => {
+      return `Row: ${e.index + 1} | Column: ${e.column} | ⚠️ Error: ${e.error}`;
+    }).join('\n')
+  ).join('\n\n');
+  const validFileList = validFiles.map(d => 
+    `📄 ${d}`).join('\n');
+  const logContent = `Updated on: ${formattedDate} UTC
+Status: ❌ Validation failed. Files reverted to their previous valid version.
+
+---
+
+🚩 Files with issues (${invalidFiles.length})
+
+${errorList}
+
+---
+
+✅ Files without any issues (${validFiles.length})
+
+${validFileList}
+
+---
+
+`;
+  fs.writeFileSync(logPath, logContent, 'utf8');
+  console.log('---');
+  console.log('');
+  console.log(`✅ validation.log updated at ${formattedDate}`);
+  console.log('');
+  process.exit(1);
+}
+
+console.log('');
+console.log(`🎉 All files are valid`);
+console.log('');
+console.log('---');
+console.log('');
+const formattedDate = new Date().toLocaleString();
+const logContent = `Updated on: ${formattedDate} UTC
+Status: ✅ Validation Successful.
+`;
+fs.writeFileSync(logPath, logContent, 'utf8');
+console.log('---');
+console.log('');
+console.log(`✅ validation.log updated at ${formattedDate}`);
+console.log('');
+process.exit(0);
